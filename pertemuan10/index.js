105d71df0eec28e80aea3c3161ed1615c5d0dfb3
KevinAldo <aldokevin419@gmail.com>
2020-09-29 12:59:58 +0800

    js8

---
+++ pertemuan10/index.js
@@ -0,0 +1,13 @@
+/*
+    Var, Let, Const
+*/
+
+let nama = "kevin";
+
+//Scope dalam Javascript
+//1. Function scope (var)
+//2. Block scope
+
+for (var i=0; i<10; i++){
+    console.log(i);
+}
---
+++ pertemuan9/index.js
@@ -0,0 +1,4 @@
+const ujian = "Hari ini Ujian ";
+let ke = 01;
+let tanggal = 17;
+console.log("Sir " + ujian + "ke-" + ke + " tanggal " + tanggal + " september 2020");
