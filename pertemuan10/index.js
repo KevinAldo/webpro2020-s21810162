eb0ad5c9e6300a095f7933ddf1bffbd2af31c04e
KevinAldo <aldokevin419@gmail.com>
2020-09-29 13:07:20 +0800

    js9

--- pertemuan10/index.js
+++ pertemuan10/index.js
@@ -2,12 +2,48 @@
     Var, Let, Const
 */
 
-let nama = "kevin";
+// let nama = "Kevin"
+// let nama = "Aldo"
 
-//Scope dalam Javascript
+// console.log(nama);
+
+//scope dalam javascript
 //1. Function scope (var)
 //2. Block scope
 
-for (var i=0; i<10; i++){
-    console.log(i);
-}
+// for (var i=0;i<10;i++)
+// {
+//     console.log(i);
+// }
+
+// function tes(){
+//     for (var i=0; i<10; i++)
+//     {
+//         console.log(i);
+//     }
+// }
+// tes();
+// console.log(i);
+
+//console.log(i);
+
+// {
+// let i;
+// for (i = 0; i<10; i++)
+// {
+//     console.log(i);
+// }
+// }
+// console.log(i);
+
+//template Literal
+
+const person = {
+    firstName: "Kevin",
+    lastName: "Aldo",
+    age: 20,
+};
+
+console.log("Hello nama saya " + person.firstName + " " + person.lastName + "." + " umur saya adalah " + person.age)
+
+console.log(`Hello nama saya ${person.firstName} ${person.lastName}. Umur saya adalah ${person.age}`);
