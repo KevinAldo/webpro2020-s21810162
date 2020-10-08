d11af027d01c6460f8f539aceabf90b9503a28cf
KevinAldo <aldokevin419@gmail.com>
2020-10-08 16:28:06 +0800

    js11

---
+++ pertemuan12/index.js
@@ -0,0 +1,36 @@
+/*
+    Arrow Function #02
+*/
+// Single argument
+// const calcAge = (year) => {
+//     return 2020 = year;
+// };
+// console.log(age(1991))
+
+// Multi Argument
+// const calcAge = (birthYear, CurrentYear) => {
+//     return CurrentYear = birthYear;
+// };
+// console.log(calcAge(1992, 2020))
+
+// No argument
+// const Ages = () => {
+//         return 2020 - 1992;
+// };
+// console.log(Ages())
+
+// No argument with Implicit Return Value
+// const calcAges = () => 2020 - 1992;
+// console.log(calcAges());
+
+// const years = [1990, 1992, 2001, 2005];
+// const calcAge1 = years.map(function(el){
+//     return 2020 - el;
+// });
+// const calc =  years.map((el) => 2020 - el)
+// console.log (calcA)
+
+// Default Parameter
+// const calcA1 = (birthYear, CurrentYear) => CurrentYear - birthYear;
+
+// console.log (calcA1(1991));
