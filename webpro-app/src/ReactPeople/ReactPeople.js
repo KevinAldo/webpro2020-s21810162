4de1079334b704b2a64f65c8813f001ab41aaa20
KevinAldo <aldokevin419@gmail.com>
2020-11-12 23:21:56 +0800

    ReactPeople

--- webpro-app/src/ReactPeople/ReactPeople.js
+++ webpro-app/src/ReactPeople/ReactPeople.js
@@ -26,7 +26,6 @@ const List = props => {
     );
 }
 
-
 const ReactPeople =() =>  {
     const human = [
         {
@@ -56,30 +55,28 @@ const ReactPeople =() =>  {
 
 export default ReactPeople;
 
-/*
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
-*/
+// export default App;
